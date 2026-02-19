f7b6f5b21ef0314335e6668b45482c8a96c9cb38
gonzalez-7 <20agonzalezp@gmail.com>
2026-02-18 20:44:48 -0800

    Add Student class with Name and Age fields

---
+++ COMP003A.LectureActivity6/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.LectureActivity6
+{
+	internal class Student
+	{
+		public string Name;
+		public int Age;
+	}
+}
